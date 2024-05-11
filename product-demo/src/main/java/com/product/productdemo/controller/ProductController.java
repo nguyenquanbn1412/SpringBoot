b690e129b4de425bc26659bf32fd9e8a6ce77fe3
nguyenquanbn1412 <148620904+nguyenquanbn1412@users.noreply.github.com>
2024-05-11 14:13:19 +0700

    Product Project

--- product-demo/src/main/java/com/product/productdemo/controller/ProductController.java
+++ product-demo/src/main/java/com/product/productdemo/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.product.productdemo.controller;
 import com.product.productdemo.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -17,9 +20,48 @@ public class ProductController {
             new Product("1239", "Xiaomi Mi 11", "Top-tier performance with innovative features at a competitive price.", 749, "Xiaomi")
     );
 
+    //BTVN Buoi 1
     @GetMapping("/products")
     public List<Product> getAllProducts() {
         return products;
     }
+
+    //BTVN Buoi 2
+     @GetMapping("/products/{id}")
+        public Product getProductById(@PathVariable String id) {
+            return products.stream()
+                    .filter(product -> product.getId().equals(id))
+                    .findFirst()
+                    .orElse(null);
+        }
+
+        @GetMapping("/products/name-starts/{prefix}")
+        public List<Product> getProductsByNamePrefix(@PathVariable String prefix) {
+            return products.stream()
+                    .filter(product -> product.getName().startsWith(prefix))
+                    .collect(Collectors.toList());
+        }
+
+        @GetMapping("/products/price/{min}/{max}")
+        public List<Product> getProductsByPriceRange(@PathVariable int min, @PathVariable int max) {
+            return products.stream()
+                    .filter(product -> product.getPrice() >= min && product.getPrice() <= max)
+                    .collect(Collectors.toList());
+        }
+
+        @GetMapping("/products/brand/{brand}")
+        public List<Product> getProductsByBrand(@PathVariable String brand) {
+            return products.stream()
+                    .filter(product -> product.getBrand().equals(brand))
+                    .collect(Collectors.toList());
+        }
+
+        @GetMapping("/products/brand/{brand}/max-price")
+        public Product getMaxPriceProductByBrand(@PathVariable String brand) {
+            return products.stream()
+                    .filter(product -> product.getBrand().equals(brand))
+                    .max(Comparator.comparing(Product::getPrice))
+                    .orElse(null);
+        }
 }
 
