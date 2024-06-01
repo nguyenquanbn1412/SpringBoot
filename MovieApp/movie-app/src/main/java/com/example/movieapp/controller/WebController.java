5af50252b7149f509ebf66ad411e5e95d076c134
nguyenquanbn1412 <148620904+nguyenquanbn1412@users.noreply.github.com>
2024-06-01 22:06:30 +0700

    mv-git

--- MovieApp/movie-app/src/main/java/com/example/movieapp/controller/WebController.java
+++ MovieApp/movie-app/src/main/java/com/example/movieapp/controller/WebController.java
@@ -1,26 +1,54 @@
 package com.example.movieapp.controller;
 
+import com.example.movieapp.entity.Movie;
+import com.example.movieapp.model.enums.MovieType;
+import com.example.movieapp.service.WebService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class WebController {
+
+    private final WebService webService;
+
+
     @GetMapping
     public String getHomePage(Model model){
         return "web/index";
     }
+    // http://localhost:8080/phim-bo
+    // http://localhost:8080/phim-bo?page=1&limit=12
     @GetMapping("/phim-bo")
-    public String getPhimBoPage(Model model){
+    public String getPhimBoPage(Model model,
+                                @RequestParam(defaultValue = "1") Integer page,
+                                @RequestParam(defaultValue = "12") Integer limit) {
+        Page<Movie> pageData = webService.findByType(MovieType.PHIM_BO, true, page, limit);
+        model.addAttribute("pageData", pageData);
+        model.addAttribute("currentPage", page);
         return "web/phim-bo";
     }
     @GetMapping("/phim-le")
-    public String getPhimLePage(Model model){
+    public String getPhimLePage(Model model,
+                                @RequestParam(defaultValue = "1") Integer page,
+                                @RequestParam(defaultValue = "12") Integer limit) {
+        Page<Movie> pageData = webService.findByType(MovieType.PHIM_LE, true, page, limit);
+        model.addAttribute("pageData", pageData);
+        model.addAttribute("currentPage", page);
         return "web/phim-le";
     }
     @GetMapping("/phim-chieu-rap")
-    public String getPhimChieuRapPage(Model model){
+    public String getPhimChieuRapPage(Model model,
+                                @RequestParam(defaultValue = "1") Integer page,
+                                @RequestParam(defaultValue = "12") Integer limit) {
+        Page<Movie> pageData = webService.findByType(MovieType.PHIM_CHIEU_RAP, true, page, limit);
+        model.addAttribute("pageData", pageData);
+        model.addAttribute("currentPage", page);
         return "web/phim-chieu-rap";
     }
     @GetMapping("/phim/{id}/{slug}")
